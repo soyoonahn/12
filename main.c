8e473d070c7c7d7cba8a6acc992dce246cce9a54
soyoonahn <70745113+soyoonahn@users.noreply.github.com>
2020-11-19 16:39:13 +0900

    ex7

--- main.c
+++ main.c
@@ -8,18 +8,30 @@ int main(int argc, char *argv[]) {
 	
 	FILE *fp;
 	char str[100];
+	char path[100];
+	char lookup[100];
+	
+	printf("input file path: ");
+	scanf("%s", path);
+	printf("input search word: ");
+	scanf("%s", lookup);
 	
 	fp = fopen("sample.txt", "r");
 	if (fp == NULL)
 	{
+		printf("invalid path! (%s)\n", path);
 		return -1;
 	}
 	
-	while(fgets(str, 100, fp) !=NULL)
-	{ 
-		printf("%s", str);
+	while( fgets(str, 100, fp) != NULL) {
+		if(strncmp(str, lookup, strlen(lookup)) ==0 )
+		{
+			printf("search succeed!\n");
+			fclose(fp);
+			return 0;		
+		}
 	}
-
+	printf("search failed!\n");
 	fclose(fp);
 	
 	return 0;
